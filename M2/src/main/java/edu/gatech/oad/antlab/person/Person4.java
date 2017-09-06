ef577f6c5dee9182ca0c40af0e6c4d21c429dce8
acsheffield <andrewcsheffield@bellsouth.net>
2017-09-06 15:06:28 -0400

    Updated Person 4, added p2 readme, and deleted useless file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Andrew Sheffield
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String newString = "";
+      for (int index = 0; index < input.length(); index++) {
+        char current = input.charAt(index);
+        current++;
+        newString += current;
+      }
+      return newString;
     }
     
     /**
