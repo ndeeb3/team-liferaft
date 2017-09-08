54e17f8d03c814b82382c2185143e8e297531038
Nichole <ndeeb3@gatech.edu>
2017-09-08 14:29:12 -0400

    Implemented calc method in Person3

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Nichole Deeb
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		String newString = "";
+		for (int i = 0; i < input.length(); i++) {
+			newString = input.charAt(i) + newString;
+		}
+		return newString;
 	}
 }
