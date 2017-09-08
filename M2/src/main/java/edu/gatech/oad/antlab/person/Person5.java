ca8365e3149b528c31749c3eb5ac93b271b37ef9
= <=>
2017-09-08 02:55:41 -0400

    person 5 implemented

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Michael Jurado
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		if (input.length() > 2)
+			return input.substring(2, input.length).concat(input.substring(0, 2));
+		return input
 	}
 	
 	/**
